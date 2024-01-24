63f47c4afa538441d164075f56b95abfe871f572
shubham <thoratshubham161@gmail.com>
2024-01-24 16:07:23 +0530

    Button fixed

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.png'
 
 const Logo = () => {
   return (
-    <img src={logo} alt='logo' width={67}/>
+    <img src={logo} alt='logo' width={67} marginLeft={100}/>
   )
 }
 
