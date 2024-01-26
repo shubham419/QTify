d254a2994ae7e13a26002117a944918946711b21
shubham419 <63647613+shubham419@users.noreply.github.com>
2024-01-26 11:28:24 +0530

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.png'
 
 const Logo = () => {
   return (
-    <img src={logo} alt='logo' width={67} height={28}/>
+    <img src={logo} alt='logo' width={67} height={37}/>
   )
 }
 
