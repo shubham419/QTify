291a54f63559e37d11f1f920dd4d25d61ee5d139
shubham <thoratshubham161@gmail.com>
2024-02-01 09:29:22 +0530

    collapes button for songs section hidden

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,45 +1,51 @@
-import React, {useState} from 'react'
-import style from './Section.module.css';
-import Card from '../Card/Card';
-import { CircularProgress } from '@mui/material';
-import Carousel from '../Carousel/Carousel';
-import SongsTabs from '../Tabs/Tabs';
+import React, { useState } from "react";
+import style from "./Section.module.css";
+import Card from "../Card/Card";
+import { CircularProgress } from "@mui/material";
+import Carousel from "../Carousel/Carousel";
+import SongsTabs from "../Tabs/Tabs";
 
-const Sectiom = ({data, type, title, handleFilter}) => {
+const Sectiom = ({ data, type, title, handleFilter }) => {
+  const [toggel, setToggel] = useState(false);
 
-
-    const [toggel, setToggel] = useState(false);
-
-    const handleToggel = () => {
-      setToggel(!toggel);
-    }
-
-  
+  const handleToggel = () => {
+    setToggel(!toggel);
+  };
 
   return (
     <div>
-        <div className={style.header}>
-          <h3>{title}</h3>
+      <div className={style.header}>
+        <h3>{title}</h3>
 
-          <h4 onClick={handleToggel} className={style.toggel_text}>{toggel ? "Collapse" : "Show all"}</h4>
-
+        {type === "song" ? null : (
+          <h4 onClick={handleToggel} className={style.toggel_text}>
+            {toggel ? "Collapse" : "Show all"}
+          </h4>
+        )}
+      </div>
+      {type === "song" ? <SongsTabs handleFilter={handleFilter} /> : null}
+      {data.length === 0 ? (
+        <CircularProgress />
+      ) : (
+        <div className={style.cards_wrapper}>
+          {toggel ? (
+            <div className={style.wrapper}>
+              {data.map((card) => {
+                return <Card data={card} type={type} key={card.id} />;
+              })}
+            </div>
+          ) : (
+            <Carousel
+              data={data}
+              renderComponent={(card) => (
+                <Card data={card} type={type} key={card.id} />
+              )}
+            />
+          )}
         </div>
-        {type==='song'? <SongsTabs handleFilter={handleFilter} /> : null}
-          {
-            data.length === 0 ? (<CircularProgress />) : (
-              <div className={style.cards_wrapper}>{
-                toggel ? (<div className={style.wrapper}>
-                    {data.map((card) => {
-                     return <Card data={card} type={type} key={card.id}/>
-                    })}
-                </div>) : (<Carousel data={data} renderComponent={(card) => <Card data={card} type={type} key={card.id}/>}/>)
-              }</div>
-            ) 
-            
-          }
-        
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Sectiom
+export default Sectiom;
